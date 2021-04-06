c6f0ff1ca53f9d2c2b969005eef6c1de8c700526
Ethan Steinberg <ethan.steinberg@gmail.com>
2021-04-06 14:22:25 -0700

    Build correctly on machines without cuda

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import torch
 
-setup(
-    name='embedding_dot',
+
+if torch.cuda.is_available():
     ext_modules=[
         CUDAExtension(
             name = 'embedding_dot_cuda', 
@@ -14,7 +15,14 @@ setup(
                 'nvcc': ['-O3', '-Xptxas', '-O3,-v'],
             },
         ),
-    ],
+    ]
+
+else:
+    ext_modules = []
+
+setup(
+    name='embedding_dot',
+    ext_modules = ext_modules,
     cmdclass={
         'build_ext': BuildExtension
     })
